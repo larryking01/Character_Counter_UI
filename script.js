797bf0c4c7592ffcd2ec8c9477db002aa4f15420
larry <larryking8118@gmail.com>
2025-04-21 01:17:20 +0000

    adding basic functionality

---
+++ script.js
@@ -0,0 +1,132 @@
+// selecting the html elements of interest.
+let textArea = document.querySelector(".text-area");
+let characterCountText = document.querySelector(".count-number-text");
+let wordCountText = document.querySelector(".word-number-text");
+let sentenceCountText = document.querySelector(".sentence-number-text");
+let approxReadingTimeText = document.querySelector(".approx-reading-time-text");
+let excludeSpacesCheckbox = document.querySelector(".checkbox");
+let characterLimitInput = document.querySelector(".character-limit-input");
+let characterLimitCheckbox = document.querySelector(".character-limit-checkbox");
+
+
+
+
+
+
+
+// function to count the number of characters in the text area.
+function countCharacters() {
+    let totalCharacters;
+    let enteredText = textArea.value;
+    let enteredTextNoSpaces = textArea.value.trim()
+                              .replace(/\s/g, '');
+
+    totalCharacters = excludeSpacesCheckbox.checked ? enteredTextNoSpaces.length : enteredText.length
+
+    let totalCharacterStringText = totalCharacters.toString()
+    if( totalCharacterStringText.length < 2 ) {
+        totalCharacterStringText = '0' + totalCharacterStringText
+    }
+    else {
+        totalCharacterStringText = totalCharacterStringText
+    }
+
+    characterCountText.textContent = totalCharacterStringText
+}
+
+
+
+// function to calculate the number of words.
+function countNumberOfWords() {
+    let enteredText = textArea.value.trim();
+    let enteredWords = enteredText.split(/\s+/).filter( word => word.length > 0 );
+
+    let numberOfWords = enteredWords.length;
+    let numberOfWordsStringText = numberOfWords.toString();
+
+    if( numberOfWordsStringText.length < 2) {
+        numberOfWordsStringText = '0' + numberOfWordsStringText
+    }
+    else {
+        numberOfWordsStringText = numberOfWordsStringText
+    }
+
+    wordCountText.textContent = numberOfWordsStringText
+
+}
+
+
+
+
+// function to calculate the number of sentences.
+function countNumberOfSentences() {
+    let enteredText = textArea.value.trim()
+
+    let sentences = enteredText.split(/(?<=[.!?])/)
+                    .map( sentence => sentence.trim())
+                    .filter( sentence => sentence.length > 0 );
+    
+    let numberOfSentences = sentences.length;
+    let numberOfSentencesStringText = numberOfSentences.toString()
+
+    if( numberOfSentencesStringText.length < 2 ) {
+        numberOfSentencesStringText = "0" + numberOfSentencesStringText
+    }
+    else {
+        numberOfSentencesStringText = numberOfSentencesStringText
+    }
+
+    sentenceCountText.textContent = numberOfSentencesStringText
+
+}
+
+
+
+// function to calculate the approx. reading time.
+function calculateApproxReadingTime() {
+    let avgWordsPerMin = 200;
+    let enteredText = textArea.value.trim();
+    let enteredWords = enteredText.split(/\s+/).filter( word => word.length > 0 );
+    let numberOfWords = enteredWords.length;
+    
+    let approxReadingTime = Math.ceil( numberOfWords / avgWordsPerMin );
+    approxReadingTimeText.textContent = `Approx. reading time: ${ approxReadingTime } min(s)`
+
+}
+
+
+
+// function to toggle character limit input visible or hidden.
+function toggleCharacterLimitInputVisible() {
+    characterLimitInput.classList.toggle("character-limit-input");
+}
+
+
+
+
+
+
+
+
+
+
+
+// adding the event handler to the text area.
+textArea.addEventListener('input', function () {
+    countCharacters();
+    countNumberOfWords();
+    countNumberOfSentences();
+    calculateApproxReadingTime();
+})
+
+
+// adding event listeners to the checkbox
+excludeSpacesCheckbox.addEventListener('change', function () {
+    countCharacters();
+})
+
+
+// adding event listeners to the character limit checkbox 
+characterLimitCheckbox.addEventListener('change', function () {
+    toggleCharacterLimitInputVisible()
+})
